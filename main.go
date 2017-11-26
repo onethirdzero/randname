5327d3e8ecf1ac403ef72d3fe53d5756372068aa
Jordan Siaw <jordansiaw.cy@gmail.com>
2017-11-26 14:31:02 -0800

    Add urfave/cli.v1

--- main.go
+++ main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+
+	cli "gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	adj := Adjectives[rand.Intn(len(Adjectives))]
-	noun := Colours[rand.Intn(len(Colours))]
+	app := cli.NewApp()
 
-	name := fmt.Sprintf("%s-%s", adj, noun)
+	app.Name = "randnamegen"
+	app.Usage = "Generate random names"
 
-	fmt.Printf("%s\n", name)
+	// The CLI app's main function.
+	app.Action = func(c *cli.Context) error {
+		adj := Adjectives[rand.Intn(len(Adjectives))]
+		noun := Colours[rand.Intn(len(Colours))]
+
+		name := fmt.Sprintf("%s-%s", adj, noun)
+
+		fmt.Printf("%s\n", name)
+
+		return nil
+	}
+
+	app.Run(os.Args)
 }
