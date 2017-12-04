566964843cb6963583d50ea42ac06ae7e5b459d6
Jordan Siaw <jordansiaw.cy@gmail.com>
2017-12-03 18:15:39 -0800

    Add support for generating camel case names

--- randname.go
+++ randname.go
@@ -10,14 +10,29 @@ import (
 // as a library, while main.go is meant to built as a binary.
 // https://stackoverflow.com/questions/14284375/can-i-have-a-library-and-binary-with-the-same-name
 
-// Generate produces a name using a random adjective and noun.
+// GenerateLowerCase produces a lower case name using a random adjective and noun.
 // delim is added as a delimiter between words.
-func Generate(delim string) string {
+func GenerateLowerCase(delim string) string {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	adj := Adjectives[rand.Intn(len(Adjectives))]
 	noun := Colours[rand.Intn(len(Colours))]
 
 	return fmt.Sprintf("%s%s%s", adj, delim, noun)
+
+}
+
+// GenerateCamelCase produces a camel case name using a random adjective and noun.
+// delim is added as a delimiter between words.
+func GenerateCamelCase(delim string) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	adj := Adjectives[rand.Intn(len(Adjectives))]
+	noun := Colours[rand.Intn(len(Colours))]
+
+	titledAdj := strings.Title(string(adj))
+	titledNoun := strings.Title(string(noun))
+
+	return fmt.Sprintf("%s%s%s", titledAdj, titledNoun, noun)
 
 }
